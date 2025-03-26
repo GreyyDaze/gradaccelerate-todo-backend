432bf9714e70a39b922227225ab4c5e8569d7fe4
Aminah Rashid <aminahrashid.bsaif20@iba-suk.edu.pk>
2025-03-27 01:48:02 +0500

    fix: added validation and made try and catch reuseab;e

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,47 +1,34 @@
 import taskService from "../services/taskService.js";
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await taskService.getAllTasks();
-    res.status(200).json(tasks);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+import asyncHandler from "../utils/asyncHandler.js";
 
-const createTask = async (req, res) => {
-  try {
-    const createdTask = await taskService.createTask(req.body);
-    res
-      .status(201)
-      .json({ data: createdTask, message: "Task created successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
 
-const updateTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updatedTask = await taskService.updateTask(parseInt(id), description);
-    res
-      .status(200)
-      .json({ data: updatedTask, message: "Task updated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+const getAllTasks = asyncHandler(async (req, res) => {
+  const tasks = await taskService.getAllTasks();
+  res.status(200).json(tasks);
+});
 
-const deleteTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedTask = await taskService.deleteTask(parseInt(id));
-    res
-      .status(200)
-      .json({ data: deletedTask, message: "Task deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+const createTask = asyncHandler(async (req, res) => {
+  const createdTask = await taskService.createTask(req.body);
+  res
+    .status(201)
+    .json({ data: createdTask, message: "Task created successfully" });
+});
+
+const updateTask = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const updatedTask = await taskService.updateTask(parseInt(id), description);
+  res
+    .status(200)
+    .json({ data: updatedTask, message: "Task updated successfully" });
+});
+
+const deleteTask = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const deletedTask = await taskService.deleteTask(parseInt(id));
+  res
+    .status(200)
+    .json({ data: deletedTask, message: "Task deleted successfully" });
+});
 
 export { getAllTasks, createTask, updateTask, deleteTask };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,12 +5,13 @@ import {
   getAllTasks,
   updateTask,
 } from "../controllers/taskController.js";
+import validateTask from "../middlewares/validateTask.js";
 
 const router = express.Router();
 
 router.get("/", getAllTasks);
-router.post("/", createTask);
-router.put("/:id", updateTask);
+router.post("/tasks", validateTask, createTask); 
+router.put("/tasks/:id", validateTask, updateTask);
 router.delete("/:id", deleteTask);
 
 export default router;
